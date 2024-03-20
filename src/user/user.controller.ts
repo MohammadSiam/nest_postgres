e518ff64624864897b6951ed34807fed6b8e109d
MohammadSiam <siamk417@gmail.com>
2024-03-20 14:52:31 +0600

    initialization done

---
+++ src/user/user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAllUser();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.viewUser(+id);
+  }
+
+  /**
+   * we have used patch decorator with id param to get id from request
+   * so the API URL will be
+   * PATCH http://localhost:3000/user/:id
+   */
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(+id, updateUserDto);
+  }
+
+  /**
+   * we have used Delete decorator with id param to get id from request
+   * so the API URL will be
+   * DELETE http://localhost:3000/user/:id
+   */
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.removeUser(+id);
+  }
+}
